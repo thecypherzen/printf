3640f0103a1c25476d68a194978288b8c3facf03
William Inyam <sw.inyam@outlook.com>
2023-09-19 21:30:22 +0000

    "Update _printf.c to skip null terminator"

--- test/main.c
+++ test/main.c
@@ -40,6 +40,9 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
+
+    printf("(pf)->\t%d | %i\n", 0xb, 0xb);
+    _printf("(_pf)->\t%d | %i\n", 0xb, 0xb);
 /*    _printf("Unknown:[%r]\n");
       printf("Unknown:[%r]\n"); */
     return (0);
